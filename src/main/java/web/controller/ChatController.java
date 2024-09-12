ec278beb68083e410e8c9729d2c24f37896836c1
yangjaeyeon05 <didwodus1234@gmail.com>
2024-09-12 11:58:33 +0900

    9/12 웹소켓

--- src/main/java/web/controller/ChatController.java
+++ src/main/java/web/controller/ChatController.java
@@ -2,16 +2,28 @@ package web.controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.List;
+import java.util.Vector;
+
 @Component  // 해당 클래스를 스프링 컨테이너에 빈 등록
 public class ChatController extends TextWebSocketHandler {
+
+    // - 클라이언트소켓들의 접속 명단을 저장하는 컬렉션 프레임워크    // ArrayList(비동기) vs Vector(동기)
+    private List<WebSocketSession> 접속된클라이언트소켓 = new Vector<>();
+
     // 1. 클라이언트가 서버 웹소켓에 접속 성공 했을 때 # Established
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("session = " + session);
         System.out.println("[서버웹소켓 측] JS웹소켓이 들어옴");
+        // - 접속된 클라이언트 소켓을 컬렉션에 저장
+        접속된클라이언트소켓.add(session);
+        // - 현재 접속된 인원수
+        System.out.println("접속인원 : " + 접속된클라이언트소켓.size());
     }
 
     // 2. 클라이언트가 서버 웹소켓에 접속 끊었을 때   # Closed
@@ -19,6 +31,25 @@ public class ChatController extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("session = " + session);
         System.out.println("[서버웹소켓 측] JS웹소켓이 나감");
+        // - 접속된 클라이언트 소켓을 접속명단에서 제외
+        접속된클라이언트소켓.remove(session);
+        // - 현재 접속된 인원수
+        System.out.println("접속인원 : " + 접속된클라이언트소켓.size());
     }
+    // 3. 클라이언트가 서버 웹소켓에 메세지를 보냈을 떄
+    // # 서버가 메세지를 받을때 이후 로직 구현
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("session = " + session + ", message = " + message);
+        System.out.println(message.getPayload());
 
+        // - 특정한 세션으로 받은 메세지 내용들 현재 접속된 다른 세션에게도 전달
+            // 1. 모든 접속된 클라이언트소켓 하나씩 꺼내기
+        for( int i = 0 ; i < 접속된클라이언트소켓.size(); i++){
+            // 2. 목록에 저장된 하나의 세션 호출
+            WebSocketSession s = 접속된클라이언트소켓.get(i);
+            // 3. 꺼낸 클라이언소켓 정보에 메세지를 보내기
+            s.sendMessage(message);
+        }
+    }
 }   // class end
--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,3 +1,28 @@
 console.log('chat.js')
 // JS 클라이언트 웹소켓 # new WebSocket("ws://localhost:8080/ws매핑주소");
 let clientSocket = new WebSocket("ws://localhost:8080/chat/conn");
+console.log(clientSocket);
+
+// [1] clientSocket 의 onclose , onerror , onmessage , onopen 정의 해야한다.
+    // (1) WebSocket 객페내 onopen 속성은 서버소켓과 접속을 성공했을 때 발동되는 함수 정의해서 대입
+clientSocket.onopen = (e) => {
+    console.log("서버소켓과 연결 성공");
+}
+    // (2) WebSocket 객페내 onmessage 속성은 서버소켓이 메세지를 보낼왔을때 발동되는 함수 정의해서 대입
+clientSocket.onmessage = (messageEvent) => {   // e : 매개변수
+    console.log(messageEvent);
+    console.log(messageEvent.data); // 받은 내용물이 들어있는 속성
+    // 1. 받은 메세지 출력할 HTML 호출
+    let msgBox = document.querySelector(".msgBox")
+    // 2. 받은 메세지를 HTML에 대입
+    msgBox.innerHTML += `<div> ${messageEvent.data} </div>`
+}
+
+// [2] 메세지 전송이벤트
+function onMsgSend(){
+    console.log('onMsgSend() called')
+    // - 현재 클라이언트 소켓과 연결 유지괸 서버소켓에게 메세지 전송
+    let msgInput = document.querySelector(".msgInput");
+    clientSocket.send(msgInput.value);
+    msgInput.value = ""
+}
